af3b69a34d21167c4b512fc8085867e8508036cc
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-06 13:25:56 +0530

    added : Setup Express Server

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
