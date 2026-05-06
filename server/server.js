f147fbd8c3642babd57bba45214941c1ad6b3c8e
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-06 13:32:15 +0530

    Added : Connect MongoDB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
-dotenv.config();
+import connectDB from "./src/config/connectDB.js";
 
+dotenv.config();
+connectDB();
 const PORT = process.env.PORT || 8082;
 
 app.listen(PORT, () => {
