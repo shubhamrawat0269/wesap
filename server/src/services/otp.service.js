4e6b57a544fa90b2793c2628843e2e83ce2b9050
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-17 12:29:05 +0530

    Added : phonenumber otp service added.

---
+++ server/src/services/otp.service.js
@@ -0,0 +1,34 @@
+import client from "../config/twilio.js";
+
+const serviceSid = process.env.TWILIO_VERIFY_SERVICE_SID;
+
+// SEND PHONE OTP
+const sendPhoneOtp = async (phoneNumber) => {
+  try {
+    console.log(`Sending OTP to this number ${phoneNumber}.`);
+    if (!phoneNumber) throw new Error("Phone Number is required.");
+    return await client.verify.v2.services(serviceSid).verifications.create({
+      to: phoneNumber,
+      channel: "sms",
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+// VERIFY PHONE OTP
+const verifyPhoneOtp = async (phoneNumber, otp) => {
+  try {
+    console.log(`Verify OTP to this number ${phoneNumber} : ${otp}`);
+    return await client.verify.v2
+      .services(serviceSid)
+      .verificationChecks.create({
+        to: phoneNumber,
+        code: otp,
+      });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+export { sendPhoneOtp, verifyPhoneOtp };
