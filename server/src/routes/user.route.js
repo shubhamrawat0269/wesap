e60ac158ed24edb7779965b325a28ef781c023d8
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-18 13:18:53 +0530

    Added : Routes added for send, verify otp

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { sendOtp } from "../controllers/user.controller.js";
+import { sendOtp, verifyOtp } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
 
 export default router;
