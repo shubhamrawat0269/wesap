c1e4187b3304c1ed486376411eba4b1c9dd6aa75
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-19 11:55:29 +0530

    Updated : schama validation changes

--- server/src/schema/user.schema.js
+++ server/src/schema/user.schema.js
@@ -4,14 +4,12 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, "Username is required"],
       trim: true,
       minlength: [3, "Username must be at least 3 characters"],
       maxlength: [30, "Username cannot exceed 30 characters"],
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
       unique: true,
       lowercase: true,
       trim: true,
