fd9b320ad40f3c1ee970829eb682757e20e97124
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-08 11:57:27 +0530

    Added : socket events: typing start, typing stop & utitlity of disconnected , added

--- server/src/services/socket.service.js
+++ server/src/services/socket.service.js
@@ -80,5 +80,143 @@ const initiazeSockets = (server) => {
         console.error("Failed to update Message");
       }
     });
+
+    socket.on("typing_start", ({ convsersationId, recieverId }) => {
+      if (!userId || !convsersationId || !recieverId) return;
+
+      if (!typingUsers.has(userId)) typingUsers.set(userId, {});
+      const userTyping = typingUsers.get(userId);
+
+      userTyping[convsersationId] = true;
+
+      // clear any exisiting timeout
+      if (userTyping[`${conversationId}_timeout`]) {
+        clearTimeout(userTyping[`${conversationId}_timeout`]);
+      }
+
+      // autostop after 3sec
+      userTyping[`${conversationId}_timeout`] = setTimeout(() => {
+        userTyping[conversationId] = false;
+        socket.to(recieverId).emit("user_typing", {
+          userId,
+          conversationId,
+          isTyping: false,
+        });
+      }, 3000);
+
+      // Notify reciever
+      socket.to(recieverId).emit("user_typing", {
+        userId,
+        conversationId,
+        isTyping: true,
+      });
+    });
+
+    socket.on("typing_stop", ({ convsersationId, recieverId }) => {
+      if (!userId || !convsersationId || !recieverId) return;
+
+      if (!typingUsers.has(userId)) {
+        const userTyping = typingUsers.get(userId);
+        userTyping[conversationId] = false;
+
+        if (userTyping[`${conversationId}_timeout`]) {
+          clearTimeout(userTyping[`${conversationId}_timeout`]);
+          delete userTyping[`${conversationId}_timeout`];
+        }
+      }
+
+      socket.to(recieverId).emit("user_typing", {
+        userId,
+        conversationId,
+        isTyping: false,
+      });
+    });
+
+    socket.on(
+      "add_reaction",
+      async ({ messageId, emoji, userId, reactionUserId }) => {
+        try {
+          const message = Message.findById(messageId);
+          if (!message) return;
+
+          const existingIndex = message.reactions.findIndex(
+            (r) => r.user.toString() === reactionUserId,
+          );
+
+          if (existingIndex > -1) {
+            const existing = messsage.reactions(existingIndex);
+            if (existing.emoji === emoji) {
+              message.reactions.splice(existingIndex, 1);
+            } else {
+              message.reactions[existingIndex].emoji = emoji;
+            }
+          } else {
+            message.reactions.push({ user: reactionUserId, emoji });
+          }
+
+          await message.save();
+          const populatedMessage = await Message.findOne(message?._id)
+            .populate("sender", "username profilePicture")
+            .populate("reciever", "username profilePicture")
+            .populate("reaction.user", "username");
+
+          const reactionUpdated = {
+            messageId,
+            reactions: populatedMessage.reactions,
+          };
+
+          const senderSocket = onlineUsers.get(
+            populatedMessage.sender?._id.toString(),
+          );
+          const recieverrSocket = onlineUsers.get(
+            populatedMessage.reciever?._id.toString(),
+          );
+
+          if (senderSocket) {
+            io.to(senderSocket).emit("reaction_update", reactionUpdated);
+          }
+
+          if (recieverSocket) {
+            io.to(recieverSocket).emit("reaction_update", reactionUpdated);
+          }
+        } catch (error) {
+          console.error(`Error Handling Reactions`, error.message);
+        }
+      },
+    );
+
+    const handleDisconnected = async () => {
+      if (!userId) return;
+
+      try {
+        onlineUsers.delete(userId);
+
+        // clear all typing timeouts
+        if (typingUsers.has(userId)) {
+          const userTyping = typingUsers.get(userId);
+          Object.keys(userTyping).forEach((key) => {
+            if (key.endsWith("_timeout")) clearTimeout(userTyping[key]);
+          });
+
+          typingUsers.delete(userId);
+        }
+
+        await User.findByIdAndUpdate(userId, {
+          isOnline: false,
+          lastSeen: new Date(),
+        });
+
+        io.emit("user_status", {
+          userId,
+          isOnline: false,
+          lastSeen: new Date(),
+        });
+
+        socket.leave(userId);
+        console.log(`user with ${userId} disconnected`);
+      } catch (error) {
+        console.error("Error in disconnecting", error.message);
+      }
+    };
   });
 };
