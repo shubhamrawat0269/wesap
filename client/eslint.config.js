c9f5042049873d3536c3e28e403f5455a29a3b1f
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-15 15:03:12 +0530

    Husky eslint prettier setup done

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,11 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      '.git/',
+      '.vscode/',
+      '*.log',
+      'package-lock.json',
+    ],
+  },
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -18,4 +28,4 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
-])
+]);
