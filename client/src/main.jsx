ce45c91ff2b1d30efc54063ac25e7ad0096c539d
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-15 16:04:31 +0530

    Added : Routing implemented

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './layouts/RootLayout'
+
+import Login from './pages/auth/Login'
+import Chat from './pages/chats/Chat'
+import NotFound from './pages/not-found/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/chat',
+        element: <Chat />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
-);
+)
