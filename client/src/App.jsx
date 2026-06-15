c1917b0e00f7f03604e32dae5efa41600005950b
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-15 15:06:04 +0530

    Deleted : Unwanted assets removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
