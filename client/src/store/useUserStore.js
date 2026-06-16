f2a641b5ce174f4a27a0de3e01d4e9bdf44071ea
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-16 16:40:10 +0530

    Added : zustand  setup done with some utilitties included

---
+++ client/src/store/useUserStore.js
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      isAuthenticated: false,
+      setUser: (userData) => set({ user: userData, isAuthenticated: true }),
+      clearUser: () => set({ user: null, isAuthenticated: false }),
+    }),
+    {
+      name: 'user-storage',
+      getStorage: () => localStorage,
+    }
+  )
+)
+
+export default useUserStore
