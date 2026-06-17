4ea8a09c8cf63018a925f56bafe1ba6cd8b8ee0c
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-17 07:57:14 +0530

    Added : validation schemas created for login form

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,4 +1,97 @@
+import * as yup from 'yup'
+import { useState } from 'react'
+import countries from '../../utils/countries'
+import { useNavigate } from 'react-router-dom'
+import useUserStore from '../../store/useUserStore'
+import useLoginStore from '../../store/useLoginStore'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const loginValidationShema = yup
+  .object()
+  .shape({
+    phoneNumber: yup
+      .string()
+      .nullable()
+      .notRequired()
+      .matches(/^\d+$/, 'Phone Number must be a digit')
+      .transform((value, originalValue) => {
+        originalValue.trim() === '' ? null : value
+      }),
+    email: yup
+      .string()
+      .nullable()
+      .notRequired()
+      .email('Please Enter Valid Email')
+      .transform((value, originalValue) => {
+        originalValue.trim() === '' ? null : value
+      }),
+  })
+  .test(
+    'at-least-one',
+    'Either email or phone number is required',
+    function (value) {
+      return !!(value.phoneNumber || value.email)
+    }
+  )
+
+const otpValidationSchema = yup.object().shape({
+  otp: yup
+    .string()
+    .length(6, 'Otp must be exactly 6 digit')
+    .required('Otp is required'),
+})
+
+const profileValidationSchema = yup.object().shape({
+  username: yup.string().required('username is required'),
+  agreed: yup.bool().oneOf([true], 'you must be agreed to the terms'),
+})
+
 const Login = () => {
+  const {
+    step,
+    setStep,
+    setUserPhoneData,
+    userPhoneData,
+    resetLoginState,
+  } = useLoginStore()
+
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(countries[0])
+  const [otp, setOtp] = useState(['', '', '', '', '', ''])
+  const [email, setEmail] = useState('')
+  const [profilePicture, setProfilePicture] = useState(null)
+  // avatar not yet included
+  const [selectedAvatar, setSelectedAvatar] = useState(avatar[0])
+  const [profilePictureFile, setProfilePictureFile] = useState(null)
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+  const { setUser } = useUserStore()
+
+  const {
+    register: loginRegister,
+    handleSubmit: handleLoginSubmit,
+    formState: { errors: loginErrors },
+  } = useForm({
+    resolver: yupResolver(loginValidationShema),
+  })
+
+  const {
+    setValue: setOtpValue,
+    handleSubmit: handleOtpSubmit,
+    formState: { errors: otpErrors },
+  } = useForm({
+    resolver: yupResolver(otpValidationSchema),
+  })
+
+  const {
+    register: profileRegister,
+    handleSubmit: handleProfileSubmit,
+    formState: { errors: profileErrors },
+    watch,
+  } = useForm({
+    resolver: yupResolver(profileValidationSchema),
+  })
+
   return <div>Login</div>
 }
 
