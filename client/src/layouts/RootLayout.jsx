860a2ec2c343b190a8293d780456232420bf4509
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-21 10:18:58 +0530

    Added : RootLayout unwanted code remove

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -3,12 +3,6 @@ import { Outlet, Link } from 'react-router-dom'
 export default function RootLayout() {
   return (
     <>
-      <nav>
-        <Link to="/login">Login</Link> | <Link to="/chat">Chat</Link>
-      </nav>
-
-      <hr />
-
       <Outlet />
     </>
   )
