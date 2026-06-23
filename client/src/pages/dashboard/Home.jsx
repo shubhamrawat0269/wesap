d4232915510a321bdcae5755050cbbde4ee6389b
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-23 18:41:40 +0530

    added : layout created

--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Layout from '../../components/Layout'
+import { motion } from 'framer-motion'
+import Chat from '../chats/Chat'
 
 const Home = () => {
-  return <div>HomePage</div>
+  return (
+    <Layout>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="h-full"
+      >
+        <Chat />
+      </motion.div>
+    </Layout>
+  )
 }
 
 export default Home
