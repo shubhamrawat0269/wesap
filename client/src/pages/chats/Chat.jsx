c47d6bf325604841303c82dc1f3192c9da7827d6
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-24 07:50:05 +0530

    updated : Chat section partial implement.

--- client/src/pages/chats/Chat.jsx
+++ client/src/pages/chats/Chat.jsx
@@ -1,5 +1,37 @@
-const Chat = () => {
-  return <div>Chat</div>
+import { useState } from 'react'
+import useLayoutStore from '../../store/useLayoutStore'
+import useThemeStore from '../../store/useThemeStore'
+import useUserStore from '../../store/useUserStore'
+
+const Chat = ({ contacts }) => {
+  const setSelectedContact = useLayoutStore(
+    (state) => state.setSelectedContact
+  )
+
+  const selectedContact = useLayoutStore(
+    (state) => state.selectedContact
+  )
+  const { theme } = useThemeStore()
+  const { user } = useUserStore()
+
+  const [searchTerms, setSearchTerms] = useState('')
+  const filteredContacts = contacts?.filter((contact) =>
+    contact?.username
+      ?.toLowerCase()
+      .includes(searchTerms.toLowerCase())
+  )
+
+  return (
+    <div
+      className={`w-full border-r h-screen ${theme == 'dark' ? 'bg-[rgb(17,27,33)] border-gray-600' : 'bg-white border-gray-200'}`}
+    >
+      <div
+        className={`p-4 flex justify-between ${theme == 'dark' ? 'text-white' : 'text-gray-800'}`}
+      >
+        <h2 className="text-xl font-semibold">Chats</h2>
+      </div>
+    </div>
+  )
 }
 
 export default Chat
--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -2,13 +2,9 @@ import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 import { motion } from 'framer-motion'
 import Chat from '../chats/Chat'
-import useLayoutStore from '../../store/useLayoutStore'
 import { getAllUsers } from '../../services/user.service'
 
 const Home = () => {
-  const setSelectedContact = useLayoutStore(
-    (state) => state.setSelectedContact
-  )
   const [allUsers, setAllUsers] = useState([])
 
   const getAllUser = async () => {
@@ -34,10 +30,7 @@ const Home = () => {
         transition={{ duration: 0.5 }}
         className="h-full"
       >
-        <Chat
-          contacts={allUsers}
-          setSelectedContact={setSelectedContact}
-        />
+        <Chat contacts={allUsers} />
       </motion.div>
     </Layout>
   )
