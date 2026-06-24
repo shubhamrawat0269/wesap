aa08985d5d621e952e41e250165058c0dbba73fd
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-24 14:29:54 +0530

    added : chat ui updated.

--- client/src/pages/chats/Chat.jsx
+++ client/src/pages/chats/Chat.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import useLayoutStore from '../../store/useLayoutStore'
 import useThemeStore from '../../store/useThemeStore'
 import useUserStore from '../../store/useUserStore'
+import { FaPlus, FaSearch } from 'react-icons/fa'
 
 const Chat = ({ contacts }) => {
   const setSelectedContact = useLayoutStore(
@@ -29,6 +30,50 @@ const Chat = ({ contacts }) => {
         className={`p-4 flex justify-between ${theme == 'dark' ? 'text-white' : 'text-gray-800'}`}
       >
         <h2 className="text-xl font-semibold">Chats</h2>
+        <button className="p-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition-colors">
+          <FaPlus />
+        </button>
+      </div>
+
+      <div className="p-2">
+        <div className="relative">
+          <FaSearch
+            className={`absolute left-3 top-1/2 transform -translate-y-1/2 ${theme == 'dark' ? 'text-gray-400' : 'text-gray-800'}`}
+          />
+          <input
+            type="text"
+            value={searchTerms}
+            placeholder="Search or start new chat"
+            className={`w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 ${theme == 'dark' ? 'bg-gray-800 text-white border-gray-700 placeholder-gray-500' : 'bg-gray-100 text-black border-gray-200 placeholder-gray-400'}`}
+            onChange={(e) => setSearchTerms(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="overflow-y-auto h-[calc(100vh-120px)]">
+        {filteredContacts.map((contact) => (
+          <motion.div
+            key={contact._id}
+            onClick={() => setSelectedContact(contact)}
+            className={`p-3 flex items-center cursor-pointer ${theme == 'dark' ? (selectedContact?._id === contact?._id ? 'bg-gray-700 hover:bg-gray-800' : selectedContact?._id === contact?._id ? 'bg-gray-700' : 'bg-gray-200') : 'hover:bg-gray-100'}`}
+          >
+            <img
+              src={contact?.profilePicture}
+              alt={contact?.username}
+              className="w-12 h-12 rounded-full"
+            />
+
+            <div className="ml-3 flex-1">
+              <div className="flex justify-between items-baseline">
+                <h2
+                  className={`font-semibold ${theme === 'dark' ? 'text-white' : 'text-black'}`}
+                >
+                  {contact?.username}
+                </h2>
+              </div>
+            </div>
+          </motion.div>
+        ))}
       </div>
     </div>
   )
